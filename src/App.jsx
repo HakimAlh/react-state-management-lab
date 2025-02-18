4288d27074b8a92bd47cfbfd4b89d9e59c2e2670
Hakim <hakimalhaddad2@gmail.com>
2025-02-18 19:27:12 +0300

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+const App = () => {
+  
+  const initialState = {
+    taskName: '',
+    category: ''
+}
+
+  return (
+    <h1>Hello world!</h1>
+  )
+}
+
+export default App
